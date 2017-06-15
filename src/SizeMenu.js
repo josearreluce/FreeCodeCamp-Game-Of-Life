0a9137dbc9772e951097deaaf488850ee5193a80
Jose Arreluce <arreluce.jose@gmail.com>
2017-06-14 21:29:28 -0500

    Add speed buttons (not functional yet)

--- src/SizeMenu.js
+++ src/SizeMenu.js
@@ -2,6 +2,52 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class SizeMenu extends React.Component {
+	render () {
+		return (
+			<div className="row">
+				<div className="col-md-8 col-md-offset-2 text-center">
+					<button className="btn btn-default"
+						id="50x30" type="button"
+						onClick={(e) => {this.props.handleClick(e)}}>
+						Size: 50x30
+					</button>
+					<button className="btn btn-default"
+						id="70x50" type="button"
+						onClick={(e) => {this.props.handleClick(e)}}>
+						Size: 70x50
+					</button>
+					<button className="btn btn-default"
+						id="100x80" type="button"
+						onClick={(e) => {this.props.handleClick(e)}}>
+						Size: 100x80
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+class SpeedMenu extends React.Component {
+	render () {
+		return (
+			<div className="row">
+				<div className="col-md-8 col-md-offset-2 text-center">
+					<button className="btn btn-default">
+						Slow
+					</button>
+					<button className="btn btn-default">
+						Medium
+					</button>
+					<button className="btn btn-default">
+						Fast
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+class SimMenu extends React.Component {
 	constructor (props) {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
@@ -21,27 +67,12 @@ class SizeMenu extends React.Component {
 
 	render () {
 		return (
-			<div className="row">
-				<div className="col-md-8 col-md-offset-2 text-center">
-					<button className="btn btn-default" 
-						id="50x30" type="button" 
-						onClick={(e) => {this.handleClick(e)}}>
-						Size: 50x30
-					</button>
-					<button className="btn btn-default" 
-						id="70x50" type="button" 
-						onClick={(e) => {this.handleClick(e)}}>
-						Size: 70x50
-					</button>
-					<button className="btn btn-default" 
-						id="100x80" type="button" 
-						onClick={(e) => {this.handleClick(e)}}>
-						Size: 100x80
-					</button>
-				</div>
+			<div>
+				<SizeMenu handleClick={this.handleClick}/>
+				<SpeedMenu />
 			</div>
 		);
 	}
 }
 
-module.exports = SizeMenu;
+module.exports = SimMenu;
